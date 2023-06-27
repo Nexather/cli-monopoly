482a3cd72d02156a84b535ad941bcfa2f062b76c
nexather <nexapig@gmail.com>
2023-06-26 21:31:42 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,24 @@ fn roll() -> i32 {
     return rand::thread_rng().gen_range(2..=12);
 }
 
+fn turn(player: &PlayerState)
+{
+    let prompt = String::from("Your turn! What will you do?\n1. Roll the Dice!    2. Build Houses/Hotels   3. Sell Houses/Hotels   4. Mortgage Properties");
+    let jail_prompt = String::from("You're in jail! What will you do? \n1. Roll for Doubles   2. Build Houses/Hotels   3. Sell Houses/Hotels   4. Mortgage Properties   5. Pay $50 Fine");
+    let mut choice_string:String = String::new();
+    let mut choice:i32 = 0;
+
+    if player.p_jailed {
+        println!("{}", jail_prompt);
+    } else {
+        println!("{}", prompt);
+    }
+
+    std::io::stdin().lock().read_line(&mut choice_string).unwrap();
+    choice = choice_string.trim().parse().expect("Input not an integer");
+    //println!("{}", choice);
+}
+
 fn main() {
     let player = PlayerState {
         p_money: 1500,
@@ -24,19 +42,15 @@ fn main() {
         p_properties: Vec::new(),
         p_jailed: false,
     };
-    let board = fs::read_to_string("src/board.txt").expect("Could not read board file");
-    let prompt = String::from("Your turn! What will you do?\n1. Roll the Dice!    2. Build Houses/Hotels   3. Sell Houses/Hotels   4. Mortgage Properties");
-    let jail_prompt = String::from("You're in jail! What will you do? \n1. Roll for Doubles   2. Pay $50 Fine   3. Build Houses/Hotels   4. Sell Houses/Hotels   5. Mortgage Properties");
 
+    let board: &str = include_str!("board.txt");
     println!("Welcome to CLI Monopoly!");
-    while true { //game running
+
+    loop { //game running
         for line in board.lines() {
             println!("{}", line);
         }
-        if player.p_jailed {
-            println!("{}", jail_prompt);
-        } else {
-            println!("{}", prompt);
-        }
+        turn(&player);
+
     }
 }
